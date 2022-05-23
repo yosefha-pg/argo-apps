e31d80615999701fade9d5fc10de70381e2caafb
Yosef Harrow <yosefha@papayaglobal.com>
2022-05-23 20:46:06 +0300

    add error protection for shell script, fix repo paths

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,14 @@
-#!/bin/bash -eu
+#!/bin/bash -e
 #set -x
 
 ENV_NAME=$1
 PROJECT=$2
+
+if [[ -z $ENV_NAME || -z $PROJECT ]]; then 
+    echo Usage: ./deploy.sh ENV_NAME PROJECT
+    exit 1 
+fi
+
+set -u
 
 kubectl apply -f <(helm template --set envName=$ENV_NAME --set project=$PROJECT charts/envs/)
